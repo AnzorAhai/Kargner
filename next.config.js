37529c01effbb1c1d634d6de404a3ace5b5dbed4
Ahai <mazinou@mail.ru>
2025-05-07 00:42:42 +0300

    Обновление 0.1.5.3. Фикс уведомлений

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const withPWA = require('next-pwa')({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
-  swSrc: 'service-worker.js'
+  swSrc: 'service-worker.js',
+  swDest: 'service-worker.js'
 });
 
 module.exports = withPWA({
